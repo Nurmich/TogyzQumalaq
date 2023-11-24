56eebb4397033c46f639d319c63b050a8de98692
Nurmukhammed Tangatarov <nurmukhammed.tangatarov@gmail.com>
2023-11-24 16:07:04 +0600

    History POST method

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'package:flutter/material.dart';
 import 'startPage.dart';
 import 'package:flutter/services.dart';
 
-void main() {WidgetsFlutterBinding.ensureInitialized();
-    SystemChrome.setPreferredOrientations([
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeRight,
     DeviceOrientation.landscapeLeft,
   ]).then((_) {
     runApp(MyApp());
   });
 }
-
 
 class mainPage extends StatelessWidget {
   @override
